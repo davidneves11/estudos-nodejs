5f9039c14a0b243229d7b08a7f94174dbf4e9bda
davidneves11 <davidneves@id.uff.br>
2022-04-30 19:27:12 -0300

    Adicionando o cadastro de um livro

--- livraria/src/app.js
+++ livraria/src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json())
+
 const livros = [
     {id: 1, titulo: "Storytelling com Dados: Um guia sobre visualização de dados para profissionais de negócios"},
     {id: 2, titulo: "Data Science para negócios"}
@@ -15,8 +17,9 @@ app.get('/livros', (req, res) => {
     res.status(200).json(livros);
 })
 
-app.get('/cadastro', (req, res) => {
-    res.status(200).send('Página de cadastro')
+app.post('/livros', (req, res) => {
+    livros.push(req.body)
+    res.status(201).send('Livro cadastrado com sucesso!')
 })
 
 export default app
